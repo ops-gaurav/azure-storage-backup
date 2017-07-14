d31d9d646fd7981c8cef491ca0902452d08253bb
gaurav <sharma02gaurav@gmail.com>
2017-07-14 03:08:46 -0700

    es6 removed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import AzureStorage from 'azure-storage';
+var AzureStorage = require('azure-storage');
 
 let exports = module.exports = {};
 
@@ -8,57 +8,57 @@ let exports = module.exports = {};
  * @return {Promise}
  */
 exports.triggerBackupBlobStorage = config => {
-	return new Promise ((resolve, reject) => {
-		let sourceBlobService = AzureStorage.createBlobService (config.source.account, config.source.accessKey);
-		
+	return new Promise((resolve, reject) => {
+		let sourceBlobService = AzureStorage.createBlobService(config.source.account, config.source.accessKey);
+
 		if (sourceBlobService) {
 
-			let targetBlobService = AzureStorage.createBlobService (config.target.account, config.target.accessKey);
+			let targetBlobService = AzureStorage.createBlobService(config.target.account, config.target.accessKey);
 
 			if (targetBlobService) {
 
-				sourceBlobService.listBlobsSegmented (config.source.container, null, (err, blobs, response) => {
-					if (err) reject (err);
+				sourceBlobService.listBlobsSegmented(config.source.container, null, (err, blobs, response) => {
+					if (err) reject(err);
 					else if (response && response.body) {
-						
+
 						let blobs = response.body.EnumerationResults.Blobs.Blob;
 						let blobNames = [];
 
-						targetBlobService.createContainerIfNotExists (config.target.container, (err, result, response) => {
+						targetBlobService.createContainerIfNotExists(config.target.container, (err, result, response) => {
 							if (!err) {
 								// console.log ();
-								for (let i=0; i < blobs.length; i++) {
+								for (let i = 0; i < blobs.length; i++) {
 									var blob = blobs[i];
 
-									let blobURI = sourceBlobService.getUrl (config.source.container, blob.Name, null, config.source.serviceEndpoint) + config.source.sasServiceParams;
-									blobNames.push ({name: val.Name, URI: blobURI});
+									let blobURI = sourceBlobService.getUrl(config.source.container, blob.Name, null, config.source.serviceEndpoint) + config.source.sasServiceParams;
+									blobNames.push({ name: val.Name, URI: blobURI });
 
 									// call the backup service to backup the current BLOB
 
-									targetBlobService.startCopyBlob (blobURI, config.target.container, 'backup_'+ val.Name, (err, blob, response) => {
-										if (err){  
-											reject (err);
-										} 
+									targetBlobService.startCopyBlob(blobURI, config.target.container, 'backup_' + val.Name, (err, blob, response) => {
+										if (err) {
+											reject(err);
+										}
 									});
 
-									if (i == blobs.length-1)
-										resolve ('Backed up '+ (i+1) +' blob(s)');
+									if (i == blobs.length - 1)
+										resolve('Backed up ' + (i + 1) + ' blob(s)');
 
 								}
 							} else {
-								reject (err);
+								reject(err);
 							}
 						})
 
-					} else reject ('No response.')
+					} else reject('No response.')
 				});
 
 			} else {
-				reject ('Connection with the target endpoint failed. Check configurations and signatures in the config object.');
+				reject('Connection with the target endpoint failed. Check configurations and signatures in the config object.');
 			}
 
 		} else {
-			reject ('Connection with the source endpoint failed. Check configurations and signatures in the config object.');
+			reject('Connection with the source endpoint failed. Check configurations and signatures in the config object.');
 		}
 	});
 }
