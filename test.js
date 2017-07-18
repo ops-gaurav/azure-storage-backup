36221a7c04657cd276e951671023c134ae795e01
gaurav <sharma02gaurav@gmail.com>
2017-07-18 17:25:09 +0530

    ignore test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-var config = require ('./tests/azure_config.js');
-var Azure = require ('./index.js')(config);
-
-Azure.triggerWholeAccountBackup().then (success => {
-	console.log (success);
-}).catch (err => {
-	console.log (err);
-})
